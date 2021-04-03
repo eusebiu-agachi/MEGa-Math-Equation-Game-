8366bc1995b574b64f0a4f8b1059a70d115b3369
BenchiaRazvan <razvanbenchia9716@gmail.com>
2021-04-03 17:10:43 +0300

    Register-popup html/css/JS

--- javascript/index.js
+++ javascript/index.js
@@ -9,5 +9,18 @@ function type(){
 	i++;
 	setTimeout(type,speed);
 }
-
 type();
+
+document.getElementById("btn-register").addEventListener("click", function(){
+	document.querySelector(".popup-register").style.display="flex";
+	document.querySelector(".navbar").style.display="none";
+})
+
+document.querySelector(".close-register").addEventListener("click", function(){
+	document.querySelector(".popup-register").style.display ="none";
+	document.querySelector(".navbar").style.display = "flex";
+})
+document.getElementById("left-register").addEventListener("click", function(){
+	document.querySelector(".popup-register").style.display="flex";
+	document.querySelector(".popup").style.display="none";
+})
