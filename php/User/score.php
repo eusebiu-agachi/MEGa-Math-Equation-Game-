86a5e9b3d6bfaa2514ec81ece2ef79da55b7848a
eusebiu-agachi <79076080+eusebiu-agachi@users.noreply.github.com>
2021-06-04 22:30:59 +0300

    scor update

---
+++ php/User/score.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once '../php/model/user.php';
+include_once '../php/database.php';
+
+
+$database = new Database();
+$db = $database->connect();
+
+
+$user = new User($db);
+
+$sql = "SELECT username, score FROM users ORDER BY score DESC LIMIT 10";
+$result = $db->prepare($sql);
+$result->execute();
+
+if($result->rowCount() >0){
+    while($row = $result->fetch(PDO::FETCH_ASSOC))
+    {
+        echo "<tr class='score-tr'><td>". $row["username"] ."</td><td>" . $row['score'] ."</td></tr> <br>";
+        
+        
+    }
+}
+
+?>
